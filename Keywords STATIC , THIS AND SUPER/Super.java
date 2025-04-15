0f9b993a99a0a9bd4c22b20d48682e4f98646a1e
Gowtham209 <gowthamts29@gmail.com>
2025-04-15 10:31:23 +0530

    Super Keyword Rule
    Child(int a,int b,String n)
        {
            super(a,b); /*ONLY one Super Per Constructor is ALLOWED*/
            super(n);
            show();
        }

--- Keywords STATIC , THIS AND SUPER/Super.java
+++ Keywords STATIC , THIS AND SUPER/Super.java
@@ -1,6 +1,6 @@
 /*Super keyword
- *Super keyword is used to refere Parent class Variables and Methods from Child class.
- * Super keyword is used to refer Parent class CONSTRUCTOR only from 
+ *Super keyword is used to refer Parent class Variables and Methods from Child class.
+ *Super keyword is used to refer Parent class CONSTRUCTOR only from 
  Child class CONSTRUCTOR should have only ONE Super call within Child CONSTRUCTOR(in 1st Line).
  * Super keyword cannot able to ACCESS Private Methods and Variables
  * Super Keyword cannot be used inside STATIC Methods
@@ -26,7 +26,7 @@ class Parent{
 class Child extends Parent{
     Child(int a,int b,String n)
     {
-        super(a,b);
+        super(a,b); /*ONLY one Super Per Constructor is ALLOWED*/
         super(n);
         show();
     }
